92d78f65293e56820765e0b2aae6eadf9302a52d
Bernik <i.am.bernik@gmail.com>
2014-11-15 14:51:31 +0200

    init lein project

---
+++ src/mustached_boo/core.clj
@@ -0,0 +1,1 @@
+(ns mustached-boo.core)
---
+++ test/mustached_boo/test/core.clj
@@ -0,0 +1,6 @@
+(ns mustached-boo.test.core
+  (:use [mustached-boo.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
